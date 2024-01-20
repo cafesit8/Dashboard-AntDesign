ea3ac1b984214aa452a9158a01512fe5fa352bfe
leliasgomer <leliasgomer@gmail.com>
2024-01-20 17:13:55 -0500

    fix: :bug: main add type

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,24 +1,33 @@
 import React, { useState } from 'react'
 import {
   ContainerOutlined,
-  DesktopOutlined,
+  BarChartOutlined,
   MailOutlined,
   PieChartOutlined
 } from '@ant-design/icons'
-import type { MenuProps } from 'antd'
 import { Button, Menu } from 'antd'
+import { useNavigate } from 'react-router-dom'
 
-type MenuItem = Required<MenuProps>['items'][number];
+type MenuItem = {
+  key: React.Key;
+  href?: string;
+  icon?: React.ReactNode;
+  children?: MenuItem[];
+  label?: React.ReactNode;
+  type?: 'group';
+};
 
 function getItem (
   label: React.ReactNode,
   key: React.Key,
+  href?: string,
   icon?: React.ReactNode,
   children?: MenuItem[],
   type?: 'group'
 ): MenuItem {
   return {
     key,
+    href,
     icon,
     children,
     label,
@@ -27,15 +36,15 @@ function getItem (
 }
 
 export const items: MenuItem[] = [
-  getItem(<span>Products</span>, '1', <PieChartOutlined />),
-  getItem('Option 2', '2', <DesktopOutlined />),
-  getItem('Option 3', '3', <ContainerOutlined />),
+  getItem('Products', '1', '/dashboard/products/list', <PieChartOutlined />),
+  getItem('Statistics', '2', '/dashboard/products/list', <BarChartOutlined />),
+  getItem('Option 3', '3', '/dashboard/products/list', <ContainerOutlined />),
 
-  getItem('Navigation One', 'sub1', <MailOutlined />, [
-    getItem('Option 5', '5'),
-    getItem('Option 6', '6'),
-    getItem('Option 7', '7'),
-    getItem('Option 8', '8')
+  getItem('Navigation One', 'sub1', '', <MailOutlined />, [
+    getItem('Option 5', '5', 'option1'),
+    getItem('Option 6', '6', 'option2'),
+    getItem('Option 7', '7', 'option3'),
+    getItem('Option 8', '8', 'option4')
   ])
 ]
 
@@ -53,9 +62,11 @@ function IconRight () {
 
 export function SideBar () {
   const [collapsed, setCollapsed] = useState(false)
+  const navigate = useNavigate()
+  const toggleCollapsed = () => setCollapsed(!collapsed)
 
-  const toggleCollapsed = () => {
-    setCollapsed(!collapsed)
+  function handleNavigate (href: string) {
+    navigate(href, { replace: true, state: { logged: true } })
   }
 
   return (
@@ -67,9 +78,22 @@ export function SideBar () {
         defaultSelectedKeys={['1']}
         mode="inline"
         inlineCollapsed={collapsed}
-        items={items}
         className={`${collapsed ? 'w-[50px] md:w-[70px]' : 'w-auto'} rounded-lg font-["Outfit_Variable"]`}
-      />
+      >
+        {items.map((item: MenuItem) => (
+          (item.children)
+            ? <Menu.SubMenu key={item.key} title={item.label} icon={item.icon}>
+              {item.children.map((childItem: MenuItem) => (
+                <Menu.Item key={childItem.key} icon={childItem.icon}>
+                  {childItem.label}
+                </Menu.Item>
+              ))}
+            </Menu.SubMenu>
+            : <Menu.Item onClick={() => handleNavigate(item.href!)} key={item.key} icon={item.icon}>
+              {item.label}
+            </Menu.Item>
+        ))}
+      </Menu>
     </div>
   )
 }
