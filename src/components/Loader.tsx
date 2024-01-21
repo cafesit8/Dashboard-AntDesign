bdc59174f6ca2457752181ac1eba6535f52a6142
leliasgomer <leliasgomer@gmail.com>
2024-01-21 16:12:35 -0500

    fix: :bug: main create loader

---
+++ src/components/Loader.tsx
@@ -0,0 +1,7 @@
+import '../styles/global.css'
+
+export default function Loader () {
+  return (
+    <div className='w-full grid place-content-center h-[300px]'><span className="loader"></span></div>
+  )
+}
