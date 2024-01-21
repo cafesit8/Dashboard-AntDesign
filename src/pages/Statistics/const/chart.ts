973a55e80d1a5a62e8bbf6fcb6d942b600c5f590
leliasgomer <leliasgomer@gmail.com>
2024-01-20 20:11:07 -0500

    fix: :bug: main create data for charts

---
+++ src/pages/Statistics/const/chart.ts
@@ -0,0 +1,106 @@
+export const chartStatistics = [
+  {
+    date: 'January',
+    Drinks: 1200,
+    Cleanliness: 543,
+    Pastas: 838
+  },
+  {
+    date: 'February',
+    Drinks: 1114,
+    Cleanliness: 623,
+    Pastas: 745
+  },
+  {
+    date: 'March',
+    Drinks: 1301,
+    Cleanliness: 598,
+    Pastas: 900
+  },
+  {
+    date: 'April',
+    Drinks: 970,
+    Cleanliness: 700,
+    Pastas: 832
+  },
+  {
+    date: 'May',
+    Drinks: 1178,
+    Cleanliness: 861,
+    Pastas: 712
+  },
+  {
+    date: 'June',
+    Drinks: 1211,
+    Cleanliness: 672,
+    Pastas: 855
+  },
+  {
+    date: 'July',
+    Drinks: 1000,
+    Cleanliness: 783,
+    Pastas: 662
+  }
+]
+
+export const chartNoSalePastas = [
+  {
+    date: 'January',
+    'No Sale': 27
+  },
+  {
+    date: 'February',
+    'No Sale': 32
+  },
+  {
+    date: 'March',
+    'No Sale': 41
+  },
+  {
+    date: 'April',
+    'No Sale': 33
+  },
+  {
+    date: 'May',
+    'No Sale': 28
+  },
+  {
+    date: 'June',
+    'No Sale': 38
+  },
+  {
+    date: 'July',
+    'No Sale': 30
+  }
+]
+
+export const chartNoSaleDrinks = [
+  {
+    date: 'January',
+    'No Sale': 57
+  },
+  {
+    date: 'February',
+    'No Sale': 62
+  },
+  {
+    date: 'March',
+    'No Sale': 41
+  },
+  {
+    date: 'April',
+    'No Sale': 56
+  },
+  {
+    date: 'May',
+    'No Sale': 70
+  },
+  {
+    date: 'June',
+    'No Sale': 62
+  },
+  {
+    date: 'July',
+    'No Sale': 46
+  }
+]
